fc85793aad7fbce06b39a37aac10840d0a5bfd61
bhumikaves-sys <bhumika.ves@gmail.com>
2026-04-21 17:53:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_0AecdFe3h ', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_0AecdFe3h', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'brctgnjqn8lrlcuu3bl8lcr7t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
